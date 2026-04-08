5c00138f265717b1ff3cc3fa962dda096bf991d0
snigdha choppa <snigchoppa@gmail.com>
2026-04-07 21:37:57 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(token) {
+  const decoded = jwt.decode(token)
+  return decoded
+}
+
+conshttps://github.com/snigdhachoppac/reviewai-test/new/maint password = "hardcoded_pass_123"
+module.exports = { verifyToken }
