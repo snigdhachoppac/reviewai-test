92c08764167284c7733c6cecffa92dc005433219
snigdha choppa <snigchoppa@gmail.com>
2026-04-07 21:48:21 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,6 +4,7 @@ function verifyToken(token) {
   const decoded = jwt.decode(token)
   return decoded
 }
+// auth module
 
 conshttps://github.com/snigdhachoppac/reviewai-test/new/maint password = "hardcoded_pass_123"
 module.exports = { verifyToken }
